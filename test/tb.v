8d5e048ec5dcb3621739b13396015435d45df8d4
rawhan10 <rohanloni50@gmail.com>
2025-02-26 21:13:30 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -6,7 +6,7 @@
 */
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
@@ -17,23 +17,21 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  wire [7:0] ui_in;
+  wire [7:0] uio_in;
+  reg [7:0] ui_in_reg;
+  reg [7:0] uio_in_reg;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_dco2 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+      .VPWR(1'b1),
+      .VGND(1'b0),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
@@ -45,5 +43,36 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
+ reg [7:0] dco_code;  
+
+  always @(*) ui_in = dco_code; // Fixed wire assignment issue
+  always #10 clk = ~clk;
+  
+  initial begin
+    clk = 0;
+    rst_n = 1;
+    ena = 0;
+    dco_code = 8'b00000000;
+    
+    #10 rst_n = 0; ena = 1;
+    #4000 dco_code = 8'b00000001;
+    #4000 dco_code = 8'b00000010;
+    #4000 dco_code = 8'b00000100;
+    #4000 dco_code = 8'b00001000;
+    #4000 dco_code = 8'b00010000;
+    #4000 dco_code = 8'b00100000;
+    #4000 dco_code = 8'b01000000;
+    #4000 dco_code = 8'b10000000;
+    
+    #10 rst_n = 1;
+    #10 rst_n = 0;
+    
+    #2000 $finish;
+  end
+  
+  initial 
+   begin
+      $monitor("Time=%0d | ui_in=%b, uo_out=%b | reset=%b | clk=%b", $time, ui_in,  uo_out, rst_n, clk);
+   end
 
 endmodule
